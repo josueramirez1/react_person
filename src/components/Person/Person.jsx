936f9c4d07f548128d3a0d1a386f68482cf03df9
Josue Ramirez <josue67ramirez@gmail.com>
2025-05-06 16:05:12 -0700

    adding age to object

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,7 +4,11 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+      {person.age ? (
+        <p className="Person__age">I am {person.age}</p>
+      ) : (
+        person.age
+      )}
       <p className="Person__partner">
         {person.isMarried ? (
           <>
